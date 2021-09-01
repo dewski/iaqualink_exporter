1365709e7b3db44b298fb490a1f8c20cc37ebc4a
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2021-09-01 11:50:05 -0700

    Add prefixes

--- app.py
+++ app.py
@@ -14,24 +14,24 @@ from iaqualink.device import (
 )
 from prometheus_client import Gauge, Enum, CONTENT_TYPE_LATEST, generate_latest
 
-spa_temp = Gauge('spa_temp', 'Description of gauge', ['system'])
-pool_temp = Gauge('pool_temp', 'Description of gauge', ['system'])
-air_temp = Gauge('air_temp', 'Description of gauge', ['system'])
+spa_temp = Gauge('iaqualink_spa_temp', 'Description of gauge', ['system'])
+pool_temp = Gauge('iaqualink_pool_temp', 'Description of gauge', ['system'])
+air_temp = Gauge('iaqualink_air_temp', 'Description of gauge', ['system'])
 
-spa_temp_target = Gauge('spa_temp_target', 'Description of gauge', ['system'])
-pool_temp_target = Gauge('pool_temp_target', 'Description of gauge', ['system'])
+spa_temp_target = Gauge('iaqualink_spa_temp_target', 'Description of gauge', ['system'])
+pool_temp_target = Gauge('iaqualink_pool_temp_target', 'Description of gauge', ['system'])
 
-spa_pump = Enum('spa_pump', 'Whether or not the Spa pump is running', ['system'], states=['running', 'stopped'])
-spa_jet_pump = Enum('spa_jet_pump', 'Whether or not the Spa jet pump is running', ['system'], states=['running', 'stopped'])
-pool_pump = Enum('pool_pump', 'Whether or not the Pool pump is running', ['system'], states=['running', 'stopped'])
-pool_cleaner_pump = Enum('pool_cleaner_pump', 'Whether or not the Pool cleaner is running', ['system'], states=['running', 'stopped'])
+spa_pump = Enum('iaqualink_spa_pump', 'Whether or not the Spa pump is running', ['system'], states=['running', 'stopped'])
+spa_jet_pump = Enum('iaqualink_spa_jet_pump', 'Whether or not the Spa jet pump is running', ['system'], states=['running', 'stopped'])
+pool_pump = Enum('iaqualink_pool_pump', 'Whether or not the Pool pump is running', ['system'], states=['running', 'stopped'])
+pool_cleaner_pump = Enum('iaqualink_pool_cleaner_pump', 'Whether or not the Pool cleaner is running', ['system'], states=['running', 'stopped'])
 
-pool_light = Enum('pool_light', 'Whether or not the Pool light is on', ['system'], states=['on', 'off'])
-spa_light = Enum('spa_light', 'Whether or not the Spa light is on', ['system'], states=['on', 'off'])
+pool_light = Enum('iaqualink_pool_light', 'Whether or not the Pool light is on', ['system'], states=['on', 'off'])
+spa_light = Enum('iaqualink_spa_light', 'Whether or not the Spa light is on', ['system'], states=['on', 'off'])
 
-spa_heater = Enum('spa_heater', 'Whether or not the Spa is heating', ['system'], states=['running', 'stopped'])
-pool_heater = Enum('pool_heater', 'Whether or not the Pool is heating', ['system'], states=['running', 'stopped'])
-solar_heater = Enum('solar_heater', 'Whether or not the Pool is being heated by Solar heater', ['system'], states=['running', 'stopped'])
+spa_heater = Enum('iaqualink_spa_heater', 'Whether or not the Spa is heating', ['system'], states=['running', 'stopped'])
+pool_heater = Enum('iaqualink_pool_heater', 'Whether or not the Pool is heating', ['system'], states=['running', 'stopped'])
+solar_heater = Enum('iaqualink_solar_heater', 'Whether or not the Pool is being heated by Solar heater', ['system'], states=['running', 'stopped'])
 
 iaqualink_user = os.environ.get('IAQUALINK_USERNAME')
 iaqualink_password = os.environ.get('IAQUALINK_PASSWORD')
